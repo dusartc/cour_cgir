bcecc6c30d7fcd802afcc1cbd24e92b1930dcebc
Clement DUSART <clement.dusart@etudiant.univ-lille1.fr>
2017-10-23 15:29:48 +0200

    ctp et ospf/rip

--- sysadmin/controle/inodes.sh
+++ sysadmin/controle/inodes.sh
@@ -5,7 +5,7 @@ test $# -eq 0 && echo "pas assez d'arguments" && exit 1
 for f;do
 	test -f $f && echo -n "inode de $f : " && stat $f -c%i && continue
 	test -h $f # && trouver l'inode du fichier cible && continue
-	test -d $f && ./$0 $f/* # appel recursif ?
+	test -d $f && ./$0 $f/* # appel recursif pour traiter le dossier
 done
 
 exit 0
